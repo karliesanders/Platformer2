bd0f4500d930f12cc86833155be32d8ddac2fbf8
karliesanders <karliesanders@gmail.com>
2020-03-26 12:34:24 -0700

    Player can jump and move

---
+++ src/Actors/Actor.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+class_name Actor
+
+const FLOOR_NORMAL: = Vector2.UP
+
+export var speed: = Vector2(300.0, 1000.0)
+export var gravity: = 3000.0 
+
+var velocity: = Vector2.ZERO
+
+#func _physics_process(delta: float) -> void:
+	#velocity.y += gravity * delta
+	# may move to each actor velocity.y = min(velocity.y, speed.y) #may change to max() 
+	
