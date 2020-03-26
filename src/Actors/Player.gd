b12740c9b457146eefac6e3abe26be815b4e1aa8
karliesanders <karliesanders@gmail.com>
2020-03-26 12:33:08 -0700

    player can jump and move

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,2 +1,26 @@
 extends Actor
 
+
+func _physics_process(delta: float) -> void:
+	var direction: = get_direction()
+	velocity = calculate_move_velocity(velocity, direction, speed)
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-1.0 if Input.is_action_just_pressed("jump")and is_on_floor() else 1.0
+	)
+	
+func calculate_move_velocity(
+		linear_velocity: Vector2,
+		direction: Vector2,
+		speed: Vector2 
+	) -> Vector2:
+	var out: = linear_velocity
+	out.x = speed.x * direction.x
+	out.y += gravity * get_physics_process_delta_time()
+	if direction.y == -1.0:
+		out.y = speed.y * direction.y
+	return out
